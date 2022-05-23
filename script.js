5030700089a90ee0ccf6fd3595593d25fe5056fb
Romkar <romkarx@gmail.com>
2022-05-23 16:50:12 +0300

    добавлено автосохранение всех полей и параметров, кнопка очистки

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// чтение из локального хранилища сразк после загрузки страницы
+document.addEventListener("DOMContentLoaded", function(event) { 
+  for (var i = 1; i < 13; i++) {
+	document.getElementById("horo-out-" + i).innerText = localStorage.getItem("horo-out-" + i);
+	document.getElementById("horo-in-" + i).value = localStorage.getItem("horo-out-" + i);
+	
+	font_size = document.getElementById("font-size-" + i);
+	font_size.value = localStorage.getItem("font-size-" + i);
+	font_size.dispatchEvent(new Event("input", { bubbles: true }));
+  }
+  
+	getdate = document.getElementById("get_date");
+	getdate.value = localStorage.getItem("get_date");
+	getdate.dispatchEvent(new Event("change", { bubbles: true }));
+});
+
+//===============================================
+// растягивание textarea по размеру текста при вводе
 const tx = document.getElementsByTagName("textarea");
 for (let i = 0; i < tx.length; i++) {
   tx[i].setAttribute(
@@ -12,10 +30,11 @@ function OnInput() {
   this.style.height = this.scrollHeight + "px";
 }
 //===============================================
-
+// обработчик событий на изменения в элементах ввода
 document.querySelectorAll("input, textarea").forEach((item) => {
   item.addEventListener("change", (event) => {
-    //-----------------------
+  //-----------------------
+  // ввод общей даты
     if (event.target.id == "get_date") {
       hor_date = (() => {
         get_date = document.getElementById("get_date").value;
@@ -38,11 +57,16 @@ document.querySelectorAll("input, textarea").forEach((item) => {
       document.querySelectorAll("[id=ht2-date]").forEach((item) => {
         item.innerHTML = hor_date;
       });
+	  
+	  localStorage.setItem("get_date", get_date);
     }
     //-----------------------
+	// ввод текстов гороскопов на вкладке "содержимое"
     for (var i = 1; i < 13; i++) {
       if (event.target.id == "horo-in-" + i) {
         document.getElementById("horo-out-" + i).innerText = event.target.value;
+		
+		    localStorage.setItem("horo-out-" + i, event.target.value);
       }
     }
     //-----------------------
@@ -50,27 +74,33 @@ document.querySelectorAll("input, textarea").forEach((item) => {
 }); // end querySelectorAll forEach
 
 //------------------------------------------------------------------
+// обработчик событий на ввод
 document.querySelectorAll("div, input").forEach((item) => {
   item.addEventListener("input", (event) => {
+	// ввод текста в карточках на вкладке "результат"
     for (var i = 1; i < 13; i++) {
       if (event.target.id == "horo-out-" + i) {
         horo_input = document.getElementById("horo-in-" + i);
         horo_input.value = event.target.innerText;
-        horo_input.dispatchEvent(new Event("input", { bubbles: true }));
+        horo_input.dispatchEvent(new Event("input", { bubbles: true })); //для увеличения области textarea
+		
+		localStorage.setItem("horo-out-" + i, event.target.innerText);
       }
     }
     //-----------------------
+	// изменение размера шрифта для карточек
     for (var i = 1; i < 13; i++) {
       if (event.target.id == "font-size-" + i) {
-        document.getElementById("horo-out-" + i).style.fontSize =
-          event.target.value + "pt";
-        document.getElementById("font-size-val-" + i).innerText =
-          event.target.value;
+        document.getElementById("horo-out-" + i).style.fontSize = event.target.value + "pt";
+        document.getElementById("font-size-val-" + i).innerText = event.target.value;
+		
+		localStorage.setItem("font-size-" + i, event.target.value);
       }
     }
   }); // end addEventListener
 }); // end querySelectorAll forEach
 //------------------------------------------------------------------
+// выгрузка всех карточек в файлы
 document.getElementById("download").addEventListener("click", (event) => {
   const signs = [
     "Овны",
@@ -104,4 +134,27 @@ document.getElementById("download").addEventListener("click", (event) => {
   download_images();
   
   
+});// end addEventListener click
+//------------------------------------------------------------------
+// очистка всех полей
+document.getElementById("delete").addEventListener("click", (event) => {
+	let isDel = confirm("Точно все удалить?");
+	if(isDel){
+	  for (var i = 1; i < 13; i++) {
+      document.getElementById("horo-out-" + i).innerText = "";
+      localStorage.setItem("horo-out-" + i, "");
+      document.getElementById("horo-in-" + i).value = "";
+      localStorage.setItem("horo-out-" + i, "");
+      
+      font_size = document.getElementById("font-size-" + i);
+	  font_size.value = 30;
+      localStorage.setItem("font-size-" + i, 30);
+      font_size.dispatchEvent(new Event("input", { bubbles: true }));
+	  }
+	  
+		getdate = document.getElementById("get_date");
+		getdate.value = "";
+		localStorage.setItem("get_date", "");
+		getdate.dispatchEvent(new Event("change", { bubbles: true }));
+	}
 });// end addEventListener click
